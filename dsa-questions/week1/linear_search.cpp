dcd67a9f7cb4426e53d88dc32e8613745ab07f83
Karan56-cyber <karanjeet4560987@gmail.com>
2026-01-21 19:53:37 +0530

    linear_search.cpp

--- dsa-questions/week1/linear_search.cpp
+++ dsa-questions/week1/linear_search.cpp
@@ -1,8 +1,35 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
+int linearsearch(vector<int>&arr,int n,int ele){
+    for(int i=0;i<n;i++){
+        if(ele==arr[i]){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
 
-int main() {
-    cout << "Week 1 DSA Started" << endl;
+    int i,n;
+    int ele;
+    vector<int>arr;
+    cout<<"enter the number of element in the array:";
+    cin>>n;
+    for(int i=0;i<n;i++){
+    
+        cin>>ele;
+        arr.push_back(ele);
+    }
+    cout<<"enter the element to be searched:";
+    cin>>ele;
+    int index=linearsearch(arr,n,ele);
+    if(index==-1){
+        cout<<"element not found in array";
+
+    }
+    else{
+        cout<<"element is found at index:"<<index;
+    }
     return 0;
 }
-
