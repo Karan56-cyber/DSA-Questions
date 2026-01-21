ff5a1ec8703bd2fa4a1e7ede6538945d19a35669
Karan56-cyber <karanjeet4560987@gmail.com>
2026-01-21 19:55:38 +0530

    Update linear_search.cpp

--- dsa-questions/week1/linear_search.cpp
+++ dsa-questions/week1/linear_search.cpp
@@ -1,35 +1,66 @@
-#include<iostream>
-#include<vector>
+/*
+Problem Name: Linear Search
+
+Problem Statement:
+Given an array of integers and an element to be searched,
+find the index of the element using Linear Search.
+If the element is not present, return -1.
+
+Input:
+- First line: number of elements (n)
+- Second line: n integers (array elements)
+- Third line: element to be searched
+
+Output:
+- Index of the element if found
+- Otherwise, print "element not found in array"
+
+Approach:
+- Traverse the array from index 0 to n-1
+- Compare each element with the target element
+- If match found, return the index
+- If traversal ends, return -1
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+#include <vector>
 using namespace std;
-int linearsearch(vector<int>&arr,int n,int ele){
-    for(int i=0;i<n;i++){
-        if(ele==arr[i]){
+
+int linearSearch(const vector<int>& arr, int ele) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == ele) {
             return i;
         }
     }
     return -1;
 }
-int main(){
 
-    int i,n;
-    int ele;
-    vector<int>arr;
-    cout<<"enter the number of element in the array:";
-    cin>>n;
-    for(int i=0;i<n;i++){
-    
-        cin>>ele;
+int main() {
+    int n, ele;
+    vector<int> arr;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    cout << "Enter the elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> ele;
         arr.push_back(ele);
     }
-    cout<<"enter the element to be searched:";
-    cin>>ele;
-    int index=linearsearch(arr,n,ele);
-    if(index==-1){
-        cout<<"element not found in array";
 
+    cout << "Enter the element to be searched: ";
+    cin >> ele;
+
+    int index = linearSearch(arr, ele);
+
+    if (index == -1) {
+        cout << "Element not found in array";
+    } else {
+        cout << "Element found at index: " << index;
     }
-    else{
-        cout<<"element is found at index:"<<index;
-    }
+
     return 0;
 }
